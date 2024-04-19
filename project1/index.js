b2db2f2a902303e24064a30e121387da43fd2773
rahulvikhe <33259870+rahulvikhe@users.noreply.github.com>
2024-04-19 20:31:00 +0530

    Added error handling & comments

--- project1/index.js
+++ project1/index.js
@@ -1,31 +1,52 @@
-var mysql      = require('mysql');
+// Importing required modules
+var mysql = require('mysql');
+var express = require('express');
+
+// Creating a MySQL connection
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'userdetails'
+  host: 'localhost',
+  user: 'root',
+  password: '', // Assuming password is empty, adjust as necessary
+  database: 'userdetails'
 });
 
-var express = require('express');
+// Checking the type of the express module
 console.log(typeof express);
 
+// Creating an Express application
 var app = express();
 
+// Handling GET request for fetching users
 app.get("/users", (req, res) => {
-    connection.query('select * from userdetails', (err, result) => {
-        if  (!err) {
+    // Querying the database to select all user details
+    connection.query('SELECT * FROM userdetails', (err, result) => {
+        // Checking for errors in the query execution
+        if (!err) {
+            // Sending the query result as response
             res.send(result);
-    }
-    })
+        } else {
+            // Sending error response if query fails
+            res.status(500).send("Error fetching users: " + err.message);
+        }
+    });
 });
 
+// Handling GET request for fetching products
 app.get("/products", (req, res) => {
-    connection.query('select * from products', (err, result) => {
-        if  (!err) {
+    // Querying the database to select all products
+    connection.query('SELECT * FROM products', (err, result) => {
+        // Checking for errors in the query execution
+        if (!err) {
+            // Sending the query result as response
             res.send(result);
-    
-    }
-    })
+        } else {
+            // Sending error response if query fails
+            res.status(500).send("Error fetching products: " + err.message);
+        }
+    });
 });
 
-app.listen(9000);
+// Listening on port 9000 for incoming requests
+app.listen(9000, () => {
+    console.log("Server is running on port 9000");
+});
